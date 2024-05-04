54fe695ee01920a6c89455fc2e3b3f5b3bb55469
sysbot <sysbot@kali>
2024-05-04 00:06:47 -0400

    fix html

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='assets')
+
 
 @app.route('/')
 def index():
-    return render_template('index-particles.html')
+    return render_template('index-triangle.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
